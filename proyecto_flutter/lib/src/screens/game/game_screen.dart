2c4fff8e11a2e6f5ba604f792e3e3aa4c391fb52
Alberto Serrano <817959@unizar.es>
2025-04-22 17:16:20 +0200

    cosas cambiadas en partidas, conexion entre dos funcionando

--- proyecto_flutter/lib/src/screens/game/game_screen.dart
+++ proyecto_flutter/lib/src/screens/game/game_screen.dart
@@ -5,9 +5,7 @@ import 'package:sota_caballo_rey/src/widgets/game/game_card.dart';
 
 class GameScreen extends StatefulWidget {
 
-  final int partidaID;
-
-  const GameScreen({super.key, required this.partidaID});
+  const GameScreen({super.key});
 
   @override
   State<GameScreen> createState() => _GameScreenState();
@@ -17,13 +15,10 @@ class _GameScreenState extends State<GameScreen> {
 
   double _volume = 0.5;
 
-  String playerName = 'Jugador 1';
-  String playerIcon = 'assets/images/app_logo_white.png';
-  String rivalName = 'Jugador 2';
-  String rivalIcon = 'assets/images/app_logo_white.png';
-
+  
 
   String triunfo = '1E';
+
   String rivalPlayedCard = '3C';
   String playerPlayedCard = '2E';
   List<String> playerHand = ['1O', '2O', '3O', '4O', '5O', '6O'];
@@ -31,6 +26,29 @@ class _GameScreenState extends State<GameScreen> {
   int puntosJugador = 0;
   int puntosRival = 0;
   int turnos = 10;
+
+  int? mazoRestante;
+    List<dynamic>? misCartas;
+    bool? faseArrastre;
+    Map<String, dynamic>? cartaTriunfo;
+    int? chatId;
+    List<dynamic>? jugadores;
+
+    // Extrae detalles de la carta triunfo
+    String? paloTriunfo;
+    int? valorTriunfo;
+
+    // Separa los datos de los jugadores
+    String? jugador1Nombre;
+    int? jugador1Equipo;
+    int? jugador1NumCartas;
+    String jugador1Icon = 'assets/images/app_logo_white.png';
+
+    String? jugador2Nombre;
+    int? jugador2Equipo;
+    int? jugador2NumCartas;
+    String jugador2Icon = 'assets/images/app_logo_white.png';
+
 
   void jugarCarta(String card) {
     setState(() {
@@ -47,6 +65,64 @@ class _GameScreenState extends State<GameScreen> {
 
   @override
   Widget build(BuildContext context) {
+    
+    /*
+    "data": {
+      "mazo_restante": 27,                                       cartas que quedan en mazo central
+      "mis_cartas": [ /* cartas asignadas al jugador */ ],       tu mano
+      "fase_arrastre": false,                                    estás en arrastre?
+      "carta_triunfo": { "palo": "oros", "valor": 7 },           carta triunfo
+      "chat_id": <CHAT_ID>,                                      id del chat de la partida
+      "jugadores": [                                             información jugadores
+        {
+          "id": 1,
+          "nombre": "Usuario 1",
+          "equipo": 1,
+          "num_cartas": 6
+        },
+        {
+          "id": 2,
+          "nombre": "Usuario 2",
+          "equipo": 2,
+          "num_cartas": 6
+        }
+      ]
+    }
+    */
+
+
+
+
+    // Obtén los argumentos pasados desde la pantalla anterior
+    final arguments = ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
+
+    // Extrae los campos del mapa "data"
+    mazoRestante = arguments?['mazo_restante'];
+    misCartas = arguments?['mis_cartas'];
+    faseArrastre = arguments?['fase_arrastre'];
+    cartaTriunfo = arguments?['carta_triunfo'];
+    chatId = arguments?['chat_id'];
+    jugadores = arguments?['jugadores'];
+
+    // Extrae detalles de la carta triunfo
+    paloTriunfo = cartaTriunfo?['palo'];
+    valorTriunfo = cartaTriunfo?['valor'];
+
+    
+
+    if (jugadores?.length != null && jugadores!.length >= 2) {
+      final jugador1 = jugadores?[0] as Map<String, dynamic>;
+      jugador1Nombre = jugador1['nombre'];
+      jugador1Equipo = jugador1['equipo'];
+      jugador1NumCartas = jugador1['num_cartas'];
+
+      final jugador2 = jugadores?[1] as Map<String, dynamic>;
+      jugador2Nombre = jugador2['nombre'];
+      jugador2Equipo = jugador2['equipo'];
+      jugador2NumCartas = jugador2['num_cartas'];
+    }
+
+
     return Scaffold(
       backgroundColor: Colors.transparent,
       body: Stack(
@@ -137,11 +213,11 @@ class _GameScreenState extends State<GameScreen> {
           // Iconos de los jugadores
           Align(
             alignment: const Alignment(-0.9, 0.48),
-            child: buildPlayerIcon(context, playerName, playerIcon),
+            child: buildPlayerIcon(context, jugador1Nombre.toString(), jugador1Icon),
           ),
           Align(
             alignment: const Alignment(0.5, -0.75),
-            child: buildPlayerIcon(context, rivalName, rivalIcon),
+            child: buildPlayerIcon(context, jugador2Nombre.toString(), jugador2Icon),
           ),
         ],
       ),
